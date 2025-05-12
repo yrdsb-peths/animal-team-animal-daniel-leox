8f6059382e3633abecf679947f2d65c6416c1f3d
Daniel-hhh <wjx200811@gmail.com>
2025-05-12 13:19:54 -0400

    resizing images

--- Elephant.java
+++ Elephant.java
@@ -10,6 +10,7 @@ public class Elephant extends Actor
         for(int i = 0; i < idle.length;i++)
         {
             idle[i] = new GreenfootImage("images/idle" + i + ".png");
+            idle[i].scale(100,100);
         }
         setImage(idle[0]);
     }
