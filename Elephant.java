1539659172fedb4c4f3194b7ed78defdf2d965cd
Daniel-hhh <wjx200811@gmail.com>
2025-05-12 13:27:52 -0400

    add timer

--- Elephant.java
+++ Elephant.java
@@ -7,6 +7,7 @@ public class Elephant extends Actor
     GreenfootImage[] idleRight = new GreenfootImage[8];
     GreenfootImage[] idleLeft = new GreenfootImage[8];
     String facing = "right";
+    SimpleTimer animationTimer = new SimpleTimer();
     public Elephant()
     {
         for(int i = 0; i < idleRight.length;i++)
@@ -20,11 +21,17 @@ public class Elephant extends Actor
             idleLeft[i].mirrorHorizontally();
             idleLeft[i].scale(100,100);
         }
+        animationTimer.mark(); 
         setImage(idleRight[0]);
     }
     int imageIndex = 0;
     public void animateElephant()
     {
+        if(animationTimer.millisElapsed() < 100)
+        {
+            return;
+        }
+        animationTimer.mark();
         if(facing.equals("right"))
         {
             setImage(idleRight[imageIndex]);
